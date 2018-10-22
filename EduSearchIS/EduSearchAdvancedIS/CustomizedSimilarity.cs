40a359bcccd7528eb089853a0f62d661ae59d936
Soam Wei Jie <soamweijie@gmail.com>
2018-10-22 14:26:11 +1000

    Custom similarity updated

--- EduSearchIS/EduSearchAdvancedIS/CustomizedSimilarity.cs
+++ EduSearchIS/EduSearchAdvancedIS/CustomizedSimilarity.cs
@@ -9,22 +9,26 @@ namespace EduSearchAdvancedIS
 {
     class CustomizedSimilarity: DefaultSimilarity
     {
-        public override float Tf(float freq)
+        public override float SloppyFreq(int distance)
         {
-            //
-            //1~4==>1*2 4~9 2*2, 2*2 9 16 3* increase the importance of the unique terms
-            //1~4 as important as 4~8
-            //4~8   2* increase the inportance of unique terms...
-
-            if (freq > 0 && freq < 4)
-            {
-                freq *= 4;
-            }
-            /*
-             * make it decrease the overall inportance of tf
-             * 
-             */
-            return (float)Math.Sqrt(freq) / 2;
+            return 1.0f / (distance + 1);
         }
+        //public override float Tf(float freq)
+        //{
+        //    //
+        //    //1~4==>1*2 4~9 2*2, 2*2 9 16 3* increase the importance of the unique terms
+        //    //1~4 as important as 4~8
+        //    //4~8   2* increase the inportance of unique terms...
+
+        //    if (freq > 0 && freq < 4)
+        //    {
+        //        freq *= 4;
+        //    }
+        //    /*
+        //     * make it decrease the overall inportance of tf
+        //     * 
+        //     */
+        //    return (float)Math.Sqrt(freq) / 2;
+        //}
     }
 }
